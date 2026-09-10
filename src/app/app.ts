597c20cae6b2b382a378c5a105bf6ed64547690e
Tarasiuk Oleksandr <tarasiuksasha2021@gmail.com>
2026-09-10 12:12:25 +0300

    Практична робота 3 - студенти

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { StudentService } from './services/student';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
+  imports: [FormsModule],
   templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('students-app');
+
+  name = '';
+  age: number | null = null;
+  grade: number | null = null;
+
+  constructor(private studentService: StudentService) {}
+
+  get students() {
+    return this.studentService.getStudents();
+  }
+
+  addStudent() {
+    if (
+      this.name.trim() &&
+      this.age !== null &&
+      this.grade !== null
+    ) {
+      this.studentService.addStudent(
+        this.name,
+        this.age,
+        this.grade
+      );
+
+      this.name = '';
+      this.age = null;
+      this.grade = null;
+    }
+  }
+
+  removeStudent(id: number) {
+    this.studentService.removeStudent(id);
+  }
 }
